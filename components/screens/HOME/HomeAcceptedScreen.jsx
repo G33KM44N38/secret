e5558a2d795116df657cb7b8d8a42b0a5b027d4f
Kylian <kylianmayanga@gmail.com>
2022-05-17 19:42:37 +0200

    add the possiblility to go to the next screen by clinking on all the screen

--- components/screens/HOME/HomeAcceptedScreen.jsx
+++ components/screens/HOME/HomeAcceptedScreen.jsx
@@ -15,7 +15,7 @@ const HomeAcceptedScreen = ({ navigation }) => {
   }
   return (
     <SafeAreaView style={{justifyContent: "center", flex: 1, alignItems: "center"}}>
-      <Pressable style={{width: "100%", alignItems: "center"}} onPress={() => validate()}>
+      <Pressable style={{width: "100%", height: "100%", alignItems: "center", justifyContent: "center"}} onPress={() => validate()}>
         <Text style={{textAlign: "center", justifyContent: "center", width: "90%"}}>
           C’est actuellement le Kylian du 23 février qui te parle, j’espère que tu vas bien, que tu as bien dormi et que tu es prête pour cette journée, bon sans plus attendre entrons dans le vif du sujet
         </Text>
